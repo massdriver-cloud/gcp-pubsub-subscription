43e946e4636bae51f6ae7f13ca39e4ead3af6038
Will <will@containerlabs.io>
2022-06-02 17:42:51 -0700

    copy pasta

--- src/main.tf
+++ src/main.tf
@@ -1,6 +1,20 @@
-resource "random_pet" "name" {
-  keepers = {
-    # An example resource w/ JSON Schema input
-    ami_id = "${var.name}"
+resource "google_pubsub_subscription" "main" {
+  name                       = var.md_metadata.name_prefix
+  topic                      = var.topic.data.infrastructure.grn
+  labels                     = var.md_metadata.default_tags
+  message_retention_duration = "${var.message_retention_duration_seconds}s"
+  retain_acked_messages      = var.retain_acked_messages
+  ack_deadline_seconds       = var.ack_deadline_seconds
+
+  # https://cloud.google.com/pubsub/docs/subscriber#lifecycle
+  # this is set to blank explicity! otherwise the subscription
+  # will expire without activity (and be deleted)
+  expiration_policy {
+    ttl = ""
   }
+  retry_policy {
+    minimum_backoff = "${var.minimum_backoff_seconds}s"
+  }
+
+  enable_message_ordering = var.enable_message_ordering
 }
---
+++ src/monitoring.tf
@@ -0,0 +1,25 @@
+
+locals {
+  threshold_backlog_bytes = var.monitoring_configuration.backlog_bytes_above
+
+  metrics = {
+    "backlog_bytes" = {
+      metric   = "pubsub.googleapis.com/subscription/backlog_bytes"
+      resource = "pubsub_subscription"
+    }
+  }
+}
+
+module "topic_bytes_alarm" {
+  count                          = var.monitoring_configuration.enabled ? 1 : 0
+  source                         = "../../../provisioners/terraform/modules/gcp-cloud-monitoring-alarm/modules/utilization-threshold"
+  md_metadata                    = var.md_metadata
+  message                        = "PubSub Subscription ${var.md_metadata.name_prefix} is above backlogBytes threshold of ${local.threshold_backlog_bytes}"
+  alarm_notification_channel_grn = var.subnetwork.data.observability.alarm_notification_channel_grn
+  alarm_name                     = "${google_pubsub_subscription.main.id}-backlogBytes"
+  metric_type                    = local.metrics["backlog_bytes"].metric
+  resource_type                  = local.metrics["backlog_bytes"].resource
+  threshold                      = local.threshold_backlog_bytes
+  period                         = 60
+  duration                       = 60
+}
