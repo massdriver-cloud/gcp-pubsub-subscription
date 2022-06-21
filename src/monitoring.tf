c813911e2e24bb27f339098cdbabf7804ad7d35c
dwill <xpositivityx@gmail.com>
2022-06-21 10:36:14 -0700

    adding package alarms

--- src/monitoring.tf
+++ src/monitoring.tf
@@ -15,9 +15,10 @@ module "alarm_channel" {
 }
 
 module "topic_bytes_alarm" {
-  source                  = "github.com/massdriver-cloud/terraform-modules//gcp-monitoring-utilization-threshold?ref=aa08797"
+  source                  = "github.com/massdriver-cloud/terraform-modules//gcp-monitoring-utilization-threshold?ref=8997456"
   notification_channel_id = module.alarm_channel.id
   md_metadata             = var.md_metadata
+  display_name            = "Message Backlog"
   message                 = "PubSub Subscription ${var.md_metadata.name_prefix} is above backlogBytes threshold of ${local.threshold_backlog_bytes}"
   alarm_name              = "${google_pubsub_subscription.main.id}-backlogBytes"
   metric_type             = local.metrics["backlog_bytes"].metric
