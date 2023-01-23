9a5a741c69afac4fa200d9632fda17c3eb011565
Will <will@containerlabs.io>
2023-01-23 15:56:27 -0800

    fresh PR

--- src/_artifacts.tf
+++ src/_artifacts.tf
@@ -7,7 +7,8 @@ locals {
       iam = {
        subscriber = {
          role      = "roles/pubsub.subscriber"
-         condition = "resource.name.endsWith(\"${var.md_metadata.name_prefix}\")"
+         # intentionally left blank, using a condition here causes issues
+         condition = ""
        }
      }
   }
@@ -30,6 +31,6 @@ locals {
 resource "massdriver_artifact" "subscription" {
   field                = "subscription"
   provider_resource_id = google_pubsub_subscription.main.id
-  name                 = "GCP PubSub Subscription ${var.md_metadata.name_prefix} (${google_pubsub_subscription.main.id})"
+  name                 = "GCP PubSub Subscription ${var.md_metadata.name_prefix}"
   artifact             = jsonencode(local.artifact_subscription)
 }
